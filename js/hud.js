7db9d7df1ffb9469a1c8e181a025692e3fa514e7
David Aikman <57766110+daikman@users.noreply.github.com>
2022-02-11 15:00:30 +0000

    update opening message and message mechanics

--- js/hud.js
+++ js/hud.js
@@ -1,12 +1,21 @@
 let hud = {
   content: [
+    [""],
     ["Hello"],
-    ["..."]
+    ["..."],
+    ["Maybe you should click over here"],
+    ["Welcome to Norrsken"],
+    ["Welcome to Norrsken, a game were you walk around in a snowy forrest."],
+    ["So..."],
+    ["Walk around!"],
   ],
+  duration: [200, 200, 100, null, 100, null, 100, 200],
   display: 0
 }
 
 function drawHUD(hudHeight) {
+
+  let hudCopy = hud
   stroke(255)
   noFill()
   // rect(16, h + 16, w - 32, 100 - 32, 16)
@@ -16,12 +25,20 @@ function drawHUD(hudHeight) {
   textFont("monospace")
   textWrap(CHAR)
   textSize(16)
-  let display = hud.content[hud.display]
-  for (let i in display) {
-    let t = display[i]
-    let w_ = (w - 64)/display.length
-    let x = 32 + i*(w - 64)/display.length
-    text(t, x, h + hudHeight/3, w_, 100)
+  let t = hudCopy.content[hudCopy.display]
+  for (let i in t) {
+    let l = t[i]
+    let w_ = (w - 64)/t.length
+    let x = 32 + i*(w - 64)/t.length
+    text(l, x, h + hudHeight/6, w_, hudHeight)
+  }
+
+  if (hudCopy.duration[hudCopy.display]) {
+    hudCopy.duration[hudCopy.display] -= 1;
+
+    if (hudCopy.duration[hudCopy.display] <= 0) {
+      nextDia()
+    }
   }
 
 }
